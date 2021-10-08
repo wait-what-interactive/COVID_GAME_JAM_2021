18afd849c294a434a3544d521ea779423d5dd85b
berun_ak20_@nuwm.edu.ua <berun_ak20_@nuwm.edu.ua>
2021-10-08 23:37:13 +0300

    Add main menu

---
+++ Assets/Scenes/SceneLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ExitTheGame()
+    {
+        Application.Quit();
+    }
+}
